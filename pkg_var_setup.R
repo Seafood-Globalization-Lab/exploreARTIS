572be65985411d8393a00d9c99f53d75b219dd61
Rahul Agrawal Bejarano <rahul.agrawal.b@gmail.com>
2022-08-02 10:40:52 -0400

    deleted section

--- pkg_var_setup.R
+++ pkg_var_setup.R
@@ -8,13 +8,5 @@ mid_east <- "#C78F0B"# Gold
 ss_afr <- "#355936" # Forest Green
 euro <- "#0686E5" # Light Blue "#00FFFF" # Cyan
 
-# e_asia_other <- paste(e_asia, trans_value, sep = "")
-# s_asia_other <- paste(s_asia, trans_value, sep = "")
-# lat_amer_other <- paste(lat_amer, trans_value, sep = "")
-# n_amer_other <- paste(n_amer, trans_value, sep = "")
-# mid_east_other <- paste(mid_east, trans_value, sep = "")
-# ss_afr_other <- paste(ss_afr, trans_value, sep = "")
-# euro_other <- paste(euro, trans_value, sep = "")
-
 # Save all variables for later use by package
 save.image(file = "R/sysdata.rda")
