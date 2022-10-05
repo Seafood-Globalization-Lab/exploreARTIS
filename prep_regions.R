f7d01f43b26291919be687adc6dfe1c138f98ec8
“rahulAgrBej” <rahul.agrawal.b@gmail.com>
2022-10-05 09:30:13 -0400

    added comments to prep_regions

--- prep_regions.R
+++ prep_regions.R
@@ -5,12 +5,15 @@ library(countrycode)
 # Preparing conversion data
 owid_regions <- read.csv("data/continents-according-to-our-world-in-data.csv")
 owid_regions <- owid_regions %>%
+  # Getting country names
   mutate(country_name = countrycode(Code, origin = "iso3c", destination = "country.name")) %>%
+  # Filter out countries that have NAs
   filter(!is.na(country_name)) %>%
   select(Code, country_name, Continent) %>%
   distinct() %>%
   rename(region = Continent)
 
+# All column names lower case
 names(owid_regions) <- tolower(names(owid_regions))
 
 write.csv(owid_regions, "data/owid_regions.csv", row.names = FALSE)
