16cf7d41d6698b7808a22e2e4c2c8bdc3340f3e3
“rahulAgrBej” <rahul.agrawal.b@gmail.com>
2022-10-10 13:58:56 -0400

    chord diagram documentation

--- R/plot_chord.R
+++ R/plot_chord.R
@@ -1,3 +1,17 @@
+#' ARTIS chord diagram
+#' 
+#' Function that creates a chord diagram from ARTIS data.
+#' 
+#' @param species list of species/species groups to include, default NA - includes all species.
+#' @param years list of years to include, default NA - includes all years.
+#' @param producers list of producers (as iso3 codes) to include, default NA - includes all producers.
+#' @param exporters list of exporters (as iso3 codes) to include, default NA - includes all exporters.
+#' @param importers list of importers (as iso3 codes) to include, default NA - includes all importers.
+#' @param hs_codes list of hs level 6 codes to include, default NA - includes all hs6 codes.
+#' @param prod_method list of production methods (capture, aquaculture, or unknown), default NA - includes all production methods.
+#' @param prod_environment list of environments (marine, inland, or unknown), default NA - includes all environments
+#' @param export_source list of types of export (domestic export, foreign export, or error export), default NA - all export sources.
+#' @param weight trade quantity type to visualize ("live" for live weight or "product" for product weight), default "live."
 #' @import tidyverse
 #' @import countrycode
 #' @import circlize
@@ -8,8 +22,9 @@ plot_chord <- function(data, focal_country = NA,
                        producers = NA, exporters = NA, importers = NA,
                        hs_codes = NA, prod_method = NA, prod_environment = NA,
                        export_source = NA, 
-                       weight = "live"){
+                       weight = "live") {
   
+  # Setting up parameters based on user input-----------------------------------
   # Select live or product weight
   if(weight == "live"){
     quantity <- "live_weight_t"
@@ -19,7 +34,16 @@ plot_chord <- function(data, focal_country = NA,
     quantity.lab <- "Quantity (t product weight)"
   }
   
-  # Filter to data selection
+  # Transparency value of flows based on focal country
+  if(sum(is.na(focal_country)) > 0){
+    # Less transparent when no focal country is selected
+    trans_value <- "80"
+  } else {
+    # For exports not destined for ISO: set to 40% transparency
+    trans_value <- "40"
+  }
+  
+  # Filtering data based on user input------------------------------------------
   data <- data %>%
     {if (sum(is.na(species)) == 0)
       filter(., sciname %in% species)
@@ -43,22 +67,13 @@ plot_chord <- function(data, focal_country = NA,
       filter(., method %in% prod_method)
       else .} %>%
     {if (sum(is.na(prod_environment)) == 0)
-      filter(., environment %in% prod_environment)
+      filter(., habitat %in% prod_environment)
       else .} %>%
     {if (sum(is.na(export_source)) == 0)
       filter(., dom_source %in% export_source)
-      else .} 
+      else .}
 
-  
-  if(sum(is.na(focal_country)) > 0){
-    # Less transparent when no focal country is selected
-    trans_value <- "80"
-  }else{
-    # For exports not destined for ISO: set to 40% transparency
-    trans_value <- "40"
-  }
-
-  # Add region columns
+  # Adding regional classification----------------------------------------------
   data <- data %>%
     mutate(importer_region = suppressWarnings(countrycode(importer_iso3c, origin = "iso3c", destination = "region")),
            exporter_region = suppressWarnings(countrycode(exporter_iso3c, origin = "iso3c", destination = "region"))) %>%
@@ -72,18 +87,21 @@ plot_chord <- function(data, focal_country = NA,
         exporter_region = case_when((exporter_iso3c %in% focal_country) ~ exporter_iso3c,
                                          TRUE ~ exporter_region))
   
+  # Re-summarizing data based on regional classification
   country_to_region <- get_country_to_region_trade(data, quantity) %>%
-    abbrev_region() 
-
-    chordDiagram(country_to_region,
-                 grid.col = sector_color_fun(country_to_region, country_iso = focal_country),
-                 col = link_transparency_fun(country_to_region, country_iso = focal_country, trans_value),
-                 order = sector_order_fun(country_to_region, country_iso = focal_country),
-                 annotationTrack = c("name", "grid"),
-                 directional = 1,
-                 direction.type = c("diffHeight", "arrows"),
-                 link.arr.type = "big.arrow",
-                 link.target.prop = FALSE,
-                 diffHeight = 0.08,
-                 link.sort = TRUE)
+    # Abbreviating region names
+    abbrev_region()
+  
+  # Creating Chord Diagram------------------------------------------------------
+  chordDiagram(country_to_region,
+               grid.col = sector_color_fun(country_to_region, country_iso = focal_country),
+               col = link_transparency_fun(country_to_region, country_iso = focal_country, trans_value),
+               order = sector_order_fun(country_to_region, country_iso = focal_country),
+               annotationTrack = c("name", "grid"),
+               directional = 1,
+               direction.type = c("diffHeight", "arrows"),
+               link.arr.type = "big.arrow",
+               link.target.prop = FALSE,
+               diffHeight = 0.08,
+               link.sort = TRUE)
 }
