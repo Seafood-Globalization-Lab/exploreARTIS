bf747ef3611bb2615ee2d72c66faac3edec23caa
jagephart <gepharja@miamioh.edu>
2022-11-22 20:58:47 -0500

    removed all functions for plot_chord that are now integrated into the main function

--- R/get_country_to_region_trade.R
+++
@@ -1,8 +0,0 @@
-#' @import tidyverse
-#' @export
-
-get_country_to_region_trade <- function(S_net, quantity) {
-  S_net %>%
-    group_by(exporter_region, importer_region) %>%
-    summarise(total_quantity = sum(.data[[quantity]], na.rm = TRUE)) 
-}
