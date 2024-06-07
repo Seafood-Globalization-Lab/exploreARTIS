e0037901d153570ec747fd5f9a4b51d952872230
theamarks <theamarks@gmail.com>
2024-06-07 15:07:04 -0700

    Create documentation for add_region()

--- R/add_region.R
+++ R/add_region.R
@@ -1,4 +1,22 @@
-#' @import tidyverse
+#' Convert country ISO3c codes to region name column
+#' 
+#' `add_region()` takes an ISO 3166-1 alpha-3 codes column and creates a new column with corresponding region names using OWID table.
+#' 
+#' @param data dataframe. Must contain chr column of ISO3c codes
+#' @param col character. Column name within data that contains ISO3c codes.
+#' @param region.col.name character. User specified name of new region name column. Default will take input col and add "_name" for the new column name.
+#' @examples
+#' 
+#' # Convert importer column codes to new column of region names
+#' glimpse(mini_artis %>% 
+#'           add_region("importer_iso3c"))
+#'           
+#' # Provide new column name
+#' glimpse(mini_artis %>% 
+#'           add_region("importer_iso3c",
+#'                       region.col.name = "test_column_name"))
+#' 
+#' @import dplyr
 #' @export
 add_region <- function(data, col, region.col.name = NA){
   if(is.na(region.col.name) == TRUE){
