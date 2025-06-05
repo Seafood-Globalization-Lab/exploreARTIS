f85c7fd97839f0e9ad8c04880e7a18e0db5fe663
Anurag Agarwal <92269948+Anurag19101996@users.noreply.github.com>
2025-06-04 22:24:37 -0700

    Delete tests/testthat/testthat.R

--- tests/testthat/testthat.R
+++
@@ -1,3 +0,0 @@
-library(testthat)         # Loads the testing framework
-library(exploreARTIS)     # Loads your package so you can test its functions
-test_check("exploreARTIS")  # Finds and runs all test-* files in tests/testthat/
